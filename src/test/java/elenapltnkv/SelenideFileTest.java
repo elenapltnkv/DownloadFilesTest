7673ca9df11a0cdd7add9473d92f70a733dad9f5
elenapltnkv <elenapltnkv@gmail.com>
2023-12-22 17:10:33 +0300

    dowload junit README.md

--- src/test/java/elenapltnkv/SelenideFileTest.java
+++ src/test/java/elenapltnkv/SelenideFileTest.java
@@ -4,17 +4,23 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 
 public class SelenideFileTest {
     @Test
 
-    public void downLoadFileTest() throws FileNotFoundException {
-        open("https://github.com/pytorch/pytorch?tab=readme-ov-file#license");
-        File downloadFile = $(By.xpath("//article/p[79]/a")).download();
+    public void downLoadFileTest() throws Exception {
+        open("https://github.com/junit-team/junit5/blob/main/README.md");
+        File downloadFile = $(".eYoABg").download();
+        InputStream ip = new FileInputStream(downloadFile);
+        byte[] bytes = ip.readAllBytes();
+        String textContents = new String(bytes, StandardCharsets.UTF_8);
+        assertThat(textContents).contains("Contributions to JUnit 5 are both welcomed");
     }
 }
